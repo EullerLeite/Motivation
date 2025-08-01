f7929b0489423a47653cbfcac73879b761e09390
EullerLeite <pgeuller3256@gmail.com>
2025-08-01 10:47:46 -0300

    Segundo commit do projeto

--- app/src/main/java/com/euller/motivation/MainActivity.kt
+++ app/src/main/java/com/euller/motivation/MainActivity.kt
@@ -1,20 +1,45 @@
 package com.euller.motivation
 
 import android.os.Bundle
+import android.view.View
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import com.euller.motivation.databinding.ActivityMainBinding
 
-class MainActivity : AppCompatActivity() {
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var bidding: ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
-        setContentView(R.layout.activity_main)
+
+        bidding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(bidding.root)
+
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setListeners()
     }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_phrase) {
+            handlePhrase()
+        }
+    }
+
+    private fun handlePhrase() {
+
+    }
+
+    private fun setListeners() {
+        bidding.buttonPhrase.setOnClickListener(this)
+    }
+
 }
