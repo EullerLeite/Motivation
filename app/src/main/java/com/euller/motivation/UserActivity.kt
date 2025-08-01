b1c6aebc1c3bbe7cc3ea2f01c0c2b4e441fb8a0f
EullerLeite <pgeuller3256@gmail.com>
2025-08-01 13:10:17 -0300

    Implementação UserActivity

---
+++ app/src/main/java/com/euller/motivation/UserActivity.kt
@@ -0,0 +1,45 @@
+package com.euller.motivation
+
+import android.os.Bundle
+import android.view.View
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import com.euller.motivation.databinding.ActivityUserBinding
+
+class UserActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var bidding: ActivityUserBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        enableEdgeToEdge()
+
+        bidding = ActivityUserBinding.inflate(layoutInflater)
+        setContentView(bidding.root)
+
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+            val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
+            insets
+        }
+
+        setListeners()
+    }
+
+    override fun onClick(v: View) {
+        if (v.id == R.id.button_save) {
+            handleSave()
+        }
+    }
+
+    private fun handleSave() {
+
+    }
+
+    private fun setListeners() {
+        bidding.buttonSave.setOnClickListener (this)
+    }
+
+}
